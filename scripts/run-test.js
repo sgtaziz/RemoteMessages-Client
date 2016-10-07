273824384bb807514c71afa19a03384e63b68969
Aziz Hasanain <sgtaziz013@gmail.com>
2016-10-07 00:38:06 -0400

    Travis-CI fix attempt #8

--- scripts/run-test.js
+++ scripts/run-test.js
@@ -2,23 +2,26 @@ var Application = require('spectron').Application
 var electron = require('electron-prebuilt')
 var assert = require('assert')
 
-var app = new Application({
-  path: electron,
-  args: ['/Users/aziz/Desktop/RemoteMessages/RemoteMessages-Client']
-})
+describe('application launch', function () {
+  this.timeout(10000)
 
-app.start().then(function () {
-  // Check if the window is visible
-  return app.browserWindow.isVisible()
-}).then(function (isVisible) {
-  // Verify the window is visible
-  assert.equal(isVisible, true)
-}).then(function () {
-  // Stop the application
-  return app.stop()
-}).catch(function (error) {
-  // Log any failures
-  console.error('Test failed', error.message)
-  app.stop();
-  process.exit(1);
+  beforeEach(function () {
+    this.app = new Application({
+      path: electron,
+	  args: [__dirname + '/../']
+    })
+    return this.app.start()
+  })
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+  })
+
+  it('shows an initial window', function () {
+    return this.app.client.getWindowCount().then(function (count) {
+      assert.equal(count, 1)
+    })
+  })
 })
